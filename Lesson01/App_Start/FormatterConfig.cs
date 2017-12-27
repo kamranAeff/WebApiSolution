ab336f0d061ecdcfff4f0f419c674d60d9494ec4
KAMRAN\KAMRAN <kamran_aeff@mail.ru>
2017-12-27 23:56:40 +0400

    ContractResolver

--- Lesson01/App_Start/FormatterConfig.cs
+++ Lesson01/App_Start/FormatterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
@@ -17,6 +18,24 @@ namespace Lesson01
 
             //json formatteri elave etmek
             config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            //CamelCasePropertyNamesContractResolver -pascalcase
+            //camelCasePropertyNamesContractResolver -camelcase
+
+            //json formatter ucun naming convertion kimi camelcase* tetbiq edilir
+            config.Formatters.JsonFormatter
+                .SerializerSettings.ContractResolver
+                = new CamelCasePropertyNamesContractResolver();
+
+
+            return;
+
+
+            //normalda XmlMediaTypeFormatter DataContractSerializer-den istifade olunur
+            //Bunu XmlSerializer`le evez etmek ucun asagidaki kodu yaza bilerik
+            //muqayiseye geldikde ise DataContractSerializer suret baximindan daha ustundur
+            config.Formatters.XmlFormatter.UseXmlSerializer = true;
+
         }
     }
 }
