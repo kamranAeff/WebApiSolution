c0c5819f3f2177eaa369deaf9b9f7ce959dddf86
KAMRAN\KAMRAN <kamran_aeff@mail.ru>
2018-01-03 14:14:58 +0400

    ok

--- Lesson01/Binders/DateTimeParameterBinder.cs
+++ Lesson01/Binders/DateTimeParameterBinder.cs
@@ -56,58 +56,6 @@ namespace Lesson01.Binders
 
             return Task.FromResult<object>(null);
         }
-
-        public DateTime? ParseDateTime(
-            string dateToParse,
-            string[] formats = null,
-            IFormatProvider provider = null,
-            DateTimeStyles styles = DateTimeStyles.AssumeLocal)
-        {
-            string[] CUSTOM_DATE_FORMATS = new string[]
-                {
-                    "yyyyMMddTHHmmssZ",
-                    "yyyyMMddTHHmmZ",
-                    "yyyyMMddTHHmmss",
-                    "yyyyMMddTHHmm",
-                    "yyyyMMddHHmmss",
-                    "yyyyMMddHHmm",
-                    "yyyyMMdd",
-                    "yyyy-MM-dd-HH-mm-ss",
-                    "yyyy-MM-dd-HH-mm",
-                    "yyyy-MM-dd",
-                    "MM-dd-yyyy"
-                };
-
-            if (formats == null)
-            {
-                formats = CUSTOM_DATE_FORMATS;
-            }
-
-            DateTime validDate;
-
-            foreach (var format in formats)
-            {
-                if (format.EndsWith("Z"))
-                {
-                    if (DateTime.TryParseExact(dateToParse, format,
-                             provider,
-                             DateTimeStyles.AssumeUniversal,
-                             out validDate))
-                    {
-                        return validDate;
-                    }
-                }
-                else
-                {
-                    if (DateTime.TryParseExact(dateToParse, format,
-                             provider, styles, out validDate))
-                    {
-                        return validDate;
-                    }
-                }
-            }
-
-            return null;
-        }
+        
     }
 }
